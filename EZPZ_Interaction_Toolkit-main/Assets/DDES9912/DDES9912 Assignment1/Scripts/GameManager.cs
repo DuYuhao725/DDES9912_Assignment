7aa3a26ec67f8cbb2d3aeb69f0abb2a2b925dbeb
DuYuhao725 <z5671836@ad.unsw.edu.au>
2026-03-17 22:28:17 +1100

    update

---
+++ EZPZ_Interaction_Toolkit-main/Assets/DDES9912/DDES9912 Assignment1/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("GameManager started successfully.");
+    }
+
+    void Update()
+    {
+        
+    }
+}
